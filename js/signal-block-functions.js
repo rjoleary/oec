c41c9cbb14ff696a73013a495cce31f6e1467b2d
Ryan O'Leary <ryan.oleary@uwaterloo.ca>
2015-02-06 23:27:08 -0500

    Add signal block functions

---
+++ js/signal-block-functions.js
@@ -0,0 +1,20 @@
+/**
+ * Front red (m) is a function of the train's speed (km/h).
+ */
+function frontRed(trainSpeed) {
+    return 50;
+}
+
+/**
+ * Front red (m) is a function of the train's speed (km/h).
+ */
+function frontYellow(trainSpeed) {
+    return 100;
+}
+
+/**
+ * Front red (m) is a function of the train's speed (km/h).
+ */
+function rearBlue(trainSpeed) {
+    return 100;
+}
