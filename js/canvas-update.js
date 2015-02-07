9b7787146aa4054d9065305e219a7d8ce3b0493f
jerryzxliu <jerryyliuu@gmail.com>
2015-02-07 02:09:01 -0500

    Final fixed

--- js/canvas-update.js
+++ js/canvas-update.js
@@ -14,33 +14,39 @@ function parseLocToCoor(front, rear, track, height, width) {
 		y = height*0.15;
 		w = height*0.01;
 		h = height*0.05;
+		loc="1-"+ (front-1000).toString();
 	} else if (track == "1W") {
 		x = width*(0.59 - (front-1501)*0.54/200);
 		y = height*0.30;
 		w = height*0.01;
 		h = height*0.05;
+		loc="1-"+ (front-1000).toString();
 	} else if (track == "2S") {
 		x = width*0.45;
 		y = height*(0.05 + (rear-2501)*0.9/(20000/75));
 		w = width*0.05;
 		h = height*0.01;
+		loc="2-"+ (front-2000).toString();
 	} else if (track == "2N") {
 		x = width*0.30;
 		y = height*(0.95 - (front-2101)*0.9/(20000/75));
 		w = width*0.05;
 		h = height*0.01;
+		loc="2-"+ (front-2000).toString();
 	} else if (track == "3E") {
 		x = width*(0.05 + (rear-3101)*0.9/(25000/75));
 		y = height*0.69;
 		w = height*0.01;
 		h = height*0.05;
+		loc="3-"+ (front-3000).toString();
 	} else if (track == "3W") {
 		x = width*(0.95 - (front-3501)*0.9/(25000/75));
 		y = height*0.84;
 		w = height*0.01;
 		h = height*0.05;
+		loc="3-"+ (front-3000).toString();
 	}
-	return [x, y, w, h];
+	return [x, y, w, h, loc];
 }
 
 /**
@@ -69,7 +75,7 @@ function updateCanvas(trainParameters) {
 	c.strokeRect(width*0.45, height*0.05, width*0.05, height*0.9);
 	c.fillText("|", width*0.475, height*0.06);
 	c.fillText("|", width*0.475, height*0.07);
-	c.fillText("V", width*0.4745, height*0.08);
+	c.fillText("V", width*0.474, height*0.08);
 	c.fillText("^", width*0.3245, height*0.93);
 	c.fillText("|", width*0.325, height*0.94);
 	c.fillText("|", width*0.325, height*0.95);
@@ -80,9 +86,6 @@ function updateCanvas(trainParameters) {
 	c.fillText("--->", width*0.05, height*0.715);
 	c.fillText("<---", width*0.94, height*0.865);
 
-	// c.fillRect(width*0.10, height*0.212, height*0.006, height*0.006);
-
-	// console.log(trainParameters[0]["trainId"]);
 
 	for(i=0; i<size; ++i) {
 		var arr = parseLocToCoor(parseInt(trainParameters[i]["front"]), parseInt(trainParameters[i]["rear"]), trainParameters[i]["track"], height, width);
@@ -95,8 +98,7 @@ function updateCanvas(trainParameters) {
 
 		if (trainParameters[i]["trainId"] == value) {
 			console.log(111);
-			$("#front").text(trainParameters[i]["front"]);
-			$("#rear").text(trainParameters[i]["rear"]);
+			$("#location").text(arr[4]);
 			$("#speed").text(trainParameters[i]["speed"]);
 			$("#state").text(trainParameters[i]["status"]);
 		}
@@ -113,17 +115,11 @@ function updateCanvas(trainParameters) {
 
 	$( "#zoom" ).click(function() {
 	 	++zoom;
-	 	if (zoom % 2 != 0)
-	 	{
-	 		// document.body.style.zoom=10.0;this.blur();
+	 	if (zoom % 2 != 0) {
 	 		$("canvas").css("transform", "scale(2,2)");
-
 	 	}
-	 	else
-	 	{
-	 		// document.body.style.zoom=1.0;this.blur();
+	 	else {
 	 		$("canvas").css("transform", "scale(1,1)");
 	 	}
-	 	console.log("CLICKED - ZOOM " + zoomtype + " ID = " + value);
 	});
 }
